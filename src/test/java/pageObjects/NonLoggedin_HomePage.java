133e065dcb55e1e5e8284a7fe7541e1446f35b1a
Suryadeepan-QA <suryadeepanc@yourdost.com>
2026-08-07 16:12:27 +0530

    Remove generated files from repository

--- src/test/java/pageObjects/NonLoggedin_HomePage.java
+++ src/test/java/pageObjects/NonLoggedin_HomePage.java
@@ -1,8 +1,12 @@
 package pageObjects;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NonLoggedin_HomePage extends BasePage{
 
@@ -15,6 +19,9 @@ public class NonLoggedin_HomePage extends BasePage{
 	
 	public void clickloginbtn()
 	{
+		  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	        wait.until(ExpectedConditions.elementToBeClickable(loginbtn));
+
 		loginbtn.click();
 	}
 }
