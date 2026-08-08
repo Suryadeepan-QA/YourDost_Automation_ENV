c7dcfd7fe748e882b3c9300d200eb8c4e0b00cfa
Suryadeepan-QA <suryadeepanc@yourdost.com>
2026-08-08 19:19:45 +0530

    logout user issue fixed

--- src/test/java/pageObjects/Logout_Page.java
+++ src/test/java/pageObjects/Logout_Page.java
@@ -1,38 +1,257 @@
 package pageObjects;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Logout_Page extends BasePage{
+public class Logout_Page extends BasePage {
 
-    WebDriverWait wait;
+    private WebDriverWait wait;
 
-	public Logout_Page(WebDriver driver) {
-		super(driver);
+    public Logout_Page(WebDriver driver) {
+        super(driver);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
-	}
-	
-	@FindBy(xpath="//div[contains(@class,'DesktopNavBar_usernameContainer')]")
-	//div[@class='DesktopNavBar_nameText__hAqgn']
-	WebElement usericon;
-	@FindBy(xpath="//li[normalize-space()='Logout']")
-	WebElement btn_logout;
+    // =========================================================
+    // LOCATORS
+    // =========================================================
 
-	public void verifyLoginSuccess()
-	{
-		wait.until(ExpectedConditions.visibilityOf(usericon));
-	}
-	public void logout_session()
-	{
-		//usericon.click();
-		//btn_logout.click();
-		   wait.until(ExpectedConditions.elementToBeClickable(usericon)).click();
-		   wait.until(ExpectedConditions.elementToBeClickable(btn_logout)).click();
-	}
+    @FindBy(xpath = "//div[contains(@class,'DesktopNavBar_usernameContainer')]")
+    private WebElement usericon;
+
+    @FindBy(xpath = "//li[normalize-space()='Logout']")
+    private WebElement btn_logout;
+
+    // =========================================================
+    // VERIFY LOGIN
+    // =========================================================
+
+    public void verifyLoginSuccess() {
+
+        System.out.println(
+                "Checking whether user is logged in..."
+        );
+
+        wait.until(
+                ExpectedConditions.visibilityOf(usericon)
+        );
+
+        System.out.println(
+                "User login verified successfully."
+        );
+    }
+
+    // =========================================================
+    // LOGOUT
+    // =========================================================
+
+    public void logout_session() {
+
+        System.out.println(
+                "================================================="
+        );
+
+        System.out.println(
+                "STARTING LOGOUT"
+        );
+
+        System.out.println(
+                "================================================="
+        );
+
+        try {
+
+            /*
+             * First check whether the username/user icon
+             * actually exists on the current page.
+             *
+             * findElements() does not throw TimeoutException.
+             */
+            By userIconLocator = By.xpath(
+                    "//div[contains(@class,'DesktopNavBar_usernameContainer')]"
+            );
+
+            List<WebElement> userIcons =
+                    driver.findElements(userIconLocator);
+
+            if (userIcons.isEmpty()) {
+
+                System.out.println(
+                        "User icon is not present on the current page."
+                );
+
+                System.out.println(
+                        "Skipping logout."
+                );
+
+                return;
+            }
+
+            /*
+             * Wait for user icon to become clickable.
+             */
+            WebElement user =
+                    wait.until(
+                            ExpectedConditions.elementToBeClickable(
+                                    userIconLocator
+                            )
+                    );
+
+            System.out.println(
+                    "User icon found."
+            );
+
+            /*
+             * Scroll into view before clicking.
+             */
+            ((org.openqa.selenium.JavascriptExecutor) driver)
+                    .executeScript(
+                            "arguments[0].scrollIntoView({"
+                                    + "block:'center',"
+                                    + "inline:'center'"
+                                    + "});",
+                            user
+                    );
+
+            /*
+             * Try normal Selenium click.
+             */
+            try {
+
+                user.click();
+
+                System.out.println(
+                        "User icon clicked."
+                );
+
+            } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+
+                System.out.println(
+                        "User icon click intercepted."
+                );
+
+                System.out.println(
+                        "Trying JavaScript click..."
+                );
+
+                ((org.openqa.selenium.JavascriptExecutor) driver)
+                        .executeScript(
+                                "arguments[0].click();",
+                                user
+                        );
+
+                System.out.println(
+                        "User icon clicked using JavaScript."
+                );
+            }
+
+            // -------------------------------------------------
+            // Wait for Logout option
+            // -------------------------------------------------
+
+            By logoutLocator = By.xpath(
+                    "//li[normalize-space()='Logout']"
+            );
+
+            List<WebElement> logoutElements =
+                    driver.findElements(logoutLocator);
+
+            if (logoutElements.isEmpty()) {
+
+                System.out.println(
+                        "Logout option is not present."
+                );
+
+                System.out.println(
+                        "Skipping logout."
+                );
+
+                return;
+            }
+
+            WebElement logoutButton =
+                    wait.until(
+                            ExpectedConditions.elementToBeClickable(
+                                    logoutLocator
+                            )
+                    );
+
+            System.out.println(
+                    "Logout option found."
+            );
+
+            /*
+             * Try normal click.
+             */
+            try {
+
+                logoutButton.click();
+
+                System.out.println(
+                        "Logout clicked successfully."
+                );
+
+            } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+
+                System.out.println(
+                        "Logout click intercepted."
+                );
+
+                System.out.println(
+                        "Trying JavaScript click..."
+                );
+
+                ((org.openqa.selenium.JavascriptExecutor) driver)
+                        .executeScript(
+                                "arguments[0].click();",
+                                logoutButton
+                        );
+
+                System.out.println(
+                        "Logout clicked using JavaScript."
+                );
+            }
+
+        } catch (Exception e) {
+
+            /*
+             * Logout is cleanup.
+             *
+             * Do not throw the exception here because a logout
+             * problem should not hide the actual result of the
+             * booking test.
+             */
+            System.out.println(
+                    "Logout could not be completed."
+            );
+
+            System.out.println(
+                    "Reason: " + e.getMessage()
+            );
+
+            System.out.println(
+                    "Continuing test cleanup..."
+            );
+        }
+
+        System.out.println(
+                "================================================="
+        );
+
+        System.out.println(
+                "LOGOUT PROCESS COMPLETED"
+        );
+
+        System.out.println(
+                "================================================="
+        );
+    }
 }
+
--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -29,86 +29,270 @@ import pageObjects.Logout_Page;
 import pageObjects.NonLoggedin_HomePage;
 
 public class BaseClass {
-	public static WebDriver driver;
-	public Logger logger;
-	public Properties p;
-	@BeforeClass
-	@Parameters({"os","browser"})
-	public void setup(String os, String br) throws IOException
-	{
-		FileReader file=new FileReader("./src//test//resources//config.properties");
-		p=new Properties();
-		p.load(file);
-		
-		logger=LogManager.getLogger(this.getClass());
-		
-		switch(br.toLowerCase())
-		{
-		case "chrome":driver=new ChromeDriver();break;
-		case "edge": 
-			    EdgeOptions options = new EdgeOptions();
-		        options.addArguments("--window-size=1920,1080");
-		        options.addArguments("--start-maximized");
-		        driver = new EdgeDriver(options);
-			break;
-		case "firefox":driver=new FirefoxDriver();break;
-		default:System.out.println("Invalid browser");return;
-		
-		}
-		driver.manage().deleteAllCookies();
-		//driver.manage().window().maximize();
-		driver.manage().window().setSize(new Dimension(1920, 1080));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get(p.getProperty("url"));
-	}
-	
-	@AfterClass
-	public void teardown()
-	{
-		driver.quit();
-	}
-	
-	public String randomstring()
-	{
-		String generatedstring=RandomStringUtils.randomAlphabetic(5);
-		return generatedstring;
-	}
-	public String randomnumber()
-	{
-		String generatednumber=RandomStringUtils.randomNumeric(5);
-	    return generatednumber;	
-	}
-	
-	public void login() throws InterruptedException, IOException {
 
-	    NonLoggedin_HomePage hp = new NonLoggedin_HomePage(driver);
-	    hp.clickloginbtn();
+    public static WebDriver driver;
 
-	    Login_Popup lp = new Login_Popup(driver);
-	    lp.setusername(p.getProperty("username"));
-	    Thread.sleep(1000);
-	    lp.setpassword(p.getProperty("password"));
-	    Thread.sleep(1000);
-	    lp.clickloginbtn();
-	}
-	
-	public void logout() {
+    public Logger logger;
 
-	    Logout_Page lp = new Logout_Page(driver);
-	    lp.logout_session();
-	}
-	
-	public static String CaptureScreen(String tname) throws IOException {
+    public Properties p;
 
-	    String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-	    TakesScreenshot ts = (TakesScreenshot) driver;
-	    File sourceFile = ts.getScreenshotAs(OutputType.FILE);
-	    String targetFilePath = System.getProperty("user.dir")
-	            + "\\screenshots\\"
-	            + tname + "_" + timeStamp + ".png";
-	    File targetFile = new File(targetFilePath);
-	    FileUtils.copyFile(sourceFile, targetFile);
-	    return targetFilePath;
-	}
+    // =========================================================
+    // SETUP
+    // =========================================================
 
+    @BeforeClass
+    @Parameters({"os", "browser"})
+    public void setup(String os, String br) throws IOException {
+
+        FileReader file =
+                new FileReader(
+                        "./src//test//resources//config.properties"
+                );
+
+        p = new Properties();
+
+        p.load(file);
+
+        file.close();
+
+        logger =
+                LogManager.getLogger(
+                        this.getClass()
+                );
+
+        switch (br.toLowerCase()) {
+
+            case "chrome":
+
+                driver = new ChromeDriver();
+
+                break;
+
+            case "edge":
+
+                EdgeOptions options =
+                        new EdgeOptions();
+
+                options.addArguments(
+                        "--window-size=1920,1080"
+                );
+
+                options.addArguments(
+                        "--start-maximized"
+                );
+
+                driver =
+                        new EdgeDriver(options);
+
+                break;
+
+            case "firefox":
+
+                driver =
+                        new FirefoxDriver();
+
+                break;
+
+            default:
+
+                System.out.println(
+                        "Invalid browser: " + br
+                );
+
+                return;
+        }
+
+        /*
+         * Delete existing cookies before starting.
+         */
+        driver.manage().deleteAllCookies();
+
+        /*
+         * Use a fixed size so local and GitHub Actions
+         * behave more consistently.
+         */
+        driver.manage().window().setSize(
+                new Dimension(1920, 1080)
+        );
+
+        /*
+         * Implicit wait.
+         */
+        driver.manage().timeouts().implicitlyWait(
+                Duration.ofSeconds(10)
+        );
+
+        /*
+         * Open application.
+         */
+        driver.get(
+                p.getProperty("url")
+        );
+    }
+
+    // =========================================================
+    // TEARDOWN
+    // =========================================================
+
+    @AfterClass
+    public void teardown() {
+
+        System.out.println(
+                "Starting browser teardown..."
+        );
+
+        try {
+
+            if (driver != null) {
+
+                driver.quit();
+
+                System.out.println(
+                        "Browser closed successfully."
+                );
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Error while closing browser: "
+                            + e.getMessage()
+            );
+        }
+    }
+
+    // =========================================================
+    // RANDOM STRING
+    // =========================================================
+
+    public String randomstring() {
+
+        String generatedstring =
+                RandomStringUtils.randomAlphabetic(5);
+
+        return generatedstring;
+    }
+
+    // =========================================================
+    // RANDOM NUMBER
+    // =========================================================
+
+    public String randomnumber() {
+
+        String generatednumber =
+                RandomStringUtils.randomNumeric(5);
+
+        return generatednumber;
+    }
+
+    // =========================================================
+    // LOGIN
+    // =========================================================
+
+    public void login()
+            throws InterruptedException, IOException {
+
+        NonLoggedin_HomePage hp =
+                new NonLoggedin_HomePage(driver);
+
+        hp.clickloginbtn();
+
+        Login_Popup lp =
+                new Login_Popup(driver);
+
+        lp.setusername(
+                p.getProperty("username")
+        );
+
+        Thread.sleep(1000);
+
+        lp.setpassword(
+                p.getProperty("password")
+        );
+
+        Thread.sleep(1000);
+
+        lp.clickloginbtn();
+    }
+
+    // =========================================================
+    // LOGOUT
+    // =========================================================
+
+    public void logout() {
+
+        System.out.println(
+                "Calling logout cleanup..."
+        );
+
+        try {
+
+            if (driver == null) {
+
+                System.out.println(
+                        "Driver is null. Logout skipped."
+                );
+
+                return;
+            }
+
+            Logout_Page lp =
+                    new Logout_Page(driver);
+
+            lp.logout_session();
+
+        } catch (Exception e) {
+
+            /*
+             * Logout is cleanup.
+             *
+             * Do not allow a cleanup problem to hide
+             * the actual test result.
+             */
+            System.out.println(
+                    "Logout cleanup failed: "
+                            + e.getMessage()
+            );
+        }
+    }
+
+    // =========================================================
+    // SCREENSHOT
+    // =========================================================
+
+    public static String CaptureScreen(
+            String tname
+    ) throws IOException {
+
+        String timeStamp =
+                new SimpleDateFormat(
+                        "yyyyMMddHHmmss"
+                ).format(new Date());
+
+        TakesScreenshot ts =
+                (TakesScreenshot) driver;
+
+        File sourceFile =
+                ts.getScreenshotAs(
+                        OutputType.FILE
+                );
+
+        String targetFilePath =
+                System.getProperty("user.dir")
+                        + "\\screenshots\\"
+                        + tname
+                        + "_"
+                        + timeStamp
+                        + ".png";
+
+        File targetFile =
+                new File(targetFilePath);
+
+        FileUtils.copyFile(
+                sourceFile,
+                targetFile
+        );
+
+        return targetFilePath;
+    }
 }
